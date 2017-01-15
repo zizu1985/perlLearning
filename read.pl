abd39fe1d36e06db98683d289c8c393d6198f264
zizu1985 <tziss85@gmail.com>
2017-01-15 12:30:49 +0100

    Example from chapter 9

---
+++ read.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+use diagnostics;
+use Data::Dumper;
+
+my %config;
+
+while (<DATA>) {
+	next if /^\s*#/; # skip comments
+	next unless /(\w+)\s*=\s*(\w+)/; # key = value
+	my ( $key, $value ) = ( $1, $2 );
+	if ( exists $config{$key} ) {
+	# we’ve already seen this key, so convert the value to an
+	# array reference
+	# Does $config{$key} currently store a scalar or an aref?
+	if( ! ref $config{$key} ) {
+		$config{$key} = [ $config{$key} ];
+	}
+		push @{ $config{$key} } => $value;
+	}
+	else {
+		$config{$key} = $value;
+	}
+}
+
+print Dumper(\%config);
+	
+__DATA__
+# max_tries = 3
+max_tries = 2 continues
+
+timeout = 30
+# only these people are OK
+user = Ovid
+user = Sally
+user = Bob
