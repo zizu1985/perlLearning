58aa877695a52e1093ae8d509fe56425ec8bbf83
zizu1985 <tziss85@gmail.com>
2017-01-25 18:39:12 +0100

    LinkExtractor example

--- LinkExtractor.pl
+++ LinkExtractor.pl
@@ -3,6 +3,7 @@ use warnings;
 
 use HTML::SimpleLinkExtor;
 use LWP::Simple 'get';
+use Carp qw(carp);
 
 # Modules below need to be installed before starting
 # cpan libwww-perl HTML::SimpleLinkExtor
@@ -19,6 +20,23 @@ unless (@links) {
 	die "No links in URL $url ";
 }
 
-foreach (@links) {
-	print "Found link : $_\n";
+
+
+sub saveLinks  {
+	my ($links , $file) = @_;
+	
+	
+	carp "File $file will be overwritten!" if -e $file;
+	
+	open(my $file_handle,">",$file) or die "Can't open file $file";
+	
+	foreach (@{ $links })
+	{
+		print $file_handle $_."\n";	
+	}
+	
+	close $file_handle or die "Can't close the $file";
+	
 }
+
+saveLinks(\@links,"links.txt");
